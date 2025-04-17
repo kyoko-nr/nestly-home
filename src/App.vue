603a2212d96895ca98a0b7f34d7903e005f4e669
kyoko-nr <kyokokitagawa@me.com>
2025-04-17 21:26:20 +0900

    add header

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,15 @@
 <script setup lang="ts">
-import HelloWorld from "./components/HelloWorld.vue";
+import HeaderComponent from "./components/header/HeaderComponent.vue";
 </script>
 
 <template>
-  <div>
-    <a
-      href="https://vite.dev"
-      target="_blank"
-      class="p-4"
-    >
-      <img
-        src="/vite.svg"
-        class="logo"
-        alt="Vite logo"
-      />
-    </a>
-    <a
-      href="https://vuejs.org/"
-      target="_blank"
-    >
-      <img
-        src="./assets/vue.svg"
-        class="logo vue"
-        alt="Vue logo"
-      />
-    </a>
+  <div
+    class="dark group/design-root bg-dark-navy relative flex size-full min-h-screen flex-col overflow-x-hidden"
+  >
+    <div class="layout-container flex h-full grow flex-col">
+      <HeaderComponent />
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
---
+++ src/components/header/HeaderComponent.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import imageIcon from "@/assets/images/icon_nestly.svg";
+
+const navItems = [
+  { label: "Vision", href: "#vision" },
+  { label: "Features", href: "#features" },
+  { label: "Experience", href: "#experience" },
+  { label: "Devices", href: "#devices" },
+  { label: "About", href: "#about" },
+  { label: "Get Started", href: "#get-started" },
+];
+</script>
+
+<template>
+  <header
+    class="border-light-gray flex h-[64px] items-center border-b px-[40px]"
+  >
+    <a
+      href="#"
+      class="flex h-full items-center gap-2"
+    >
+      <img
+        class="size-4"
+        :src="imageIcon"
+        alt=""
+      />
+      <h2
+        class="text-lg leading-tight font-bold tracking-[-0.015em] text-white"
+      >
+        NESTLY
+      </h2>
+    </a>
+    <div class="flex h-full flex-1 justify-end gap-8">
+      <ul class="flex items-center gap-[16px]">
+        <li
+          v-for="item in navItems"
+          :key="item.href"
+          class="h-full"
+        >
+          <a
+            :href="item.href"
+            class="flex h-full items-center px-[12px] text-sm leading-normal font-medium text-white"
+            ><span>{{ item.label }}</span></a
+          >
+        </li>
+      </ul>
+    </div>
+  </header>
+</template>
