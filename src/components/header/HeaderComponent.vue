aa33bd07b665172cb8f252a21b48848c5d6c69a1
kyoko-nr <kyokokitagawa@me.com>
2025-04-26 17:48:36 +0900

    add header's hover animation

--- src/components/header/HeaderComponent.vue
+++ src/components/header/HeaderComponent.vue
@@ -59,11 +59,39 @@ onMounted(() => {
         >
           <a
             :href="item.href"
-            class="flex h-full items-center px-[20px] leading-normal"
-            ><span class="text-base tracking-wider">{{ item.label }}</span></a
+            class="custom-link flex h-full items-center px-[8px] leading-normal"
+            ><span
+              class="custom-text b relative px-[8px] py-[2px] text-base tracking-wider"
+              >{{ item.label }}</span
+            ></a
           >
         </li>
       </ul>
     </div>
   </header>
 </template>
+
+<style lang="css" scoped>
+.custom-text::before {
+  content: "";
+  background-image: linear-gradient(var(--color-primary), var(--color-primary));
+  background-repeat: no-repeat;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+  background-size: 0% 100%;
+  transition: background-size 0.4s var(--ease-in-out-expo);
+  background-position: bottom right;
+  opacity: 0.8;
+}
+
+@media (any-hover: hover) {
+  .custom-link:hover .custom-text::before {
+    background-size: 100% 100%;
+    background-position: bottom left;
+  }
+}
+</style>
