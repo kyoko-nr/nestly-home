8f49debe49dd86a57e5e7d8254289119df2a609b
kyoko-nr <kyokokitagawa@me.com>
2025-04-26 16:41:13 +0900

    add gsap, loading animation

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { onMounted, ref } from "vue";
 import CalloutComponent from "./components/callout/CalloutComponent.vue";
 import ExperienceComponent from "./components/experience/ExperienceComponent.vue";
 import FaqComponent from "./components/faq/FaqComponent.vue";
@@ -6,24 +7,43 @@ import FeaturesComponent from "./components/features/FeaturesComponent.vue";
 import HeaderComponent from "./components/header/HeaderComponent.vue";
 import HeroComponent from "./components/hero/HeroComponent.vue";
 import VisionComponent from "./components/vision/VisionComponent.vue";
+import LoadingComponent from "./components/loading/LoadingComponent.vue";
+
+const isLoading = ref(true);
+
+onMounted(() => {
+  setTimeout(() => {
+    isLoading.value = false;
+  }, 3000);
+});
 </script>
 
 <template>
-  <div
-    class="dark group/design-root text-dark-gray relative flex size-full min-h-screen flex-col overflow-x-hidden"
-  >
-    <div class="layout-container flex h-full grow flex-col">
-      <HeaderComponent />
-      <main>
-        <HeroComponent />
-        <VisionComponent />
-        <FeaturesComponent />
-        <ExperienceComponent />
-        <FaqComponent />
-        <CalloutComponent />
-      </main>
+  <div class="relative">
+    <div
+      v-if="isLoading"
+      class="absolute h-full w-full"
+    >
+      <LoadingComponent :is-loading="isLoading" />
+    </div>
+    <div
+      class="dark group/design-root text-dark-gray relative flex size-full min-h-screen flex-col overflow-x-hidden"
+      :class="{ 'overflow-hidden': isLoading, 'h-screen': isLoading }"
+    >
+      <div
+        class="layout-container flex h-full grow flex-col"
+        :class="{ 'overflow-hidden': isLoading }"
+      >
+        <HeaderComponent />
+        <main>
+          <HeroComponent />
+          <VisionComponent />
+          <FeaturesComponent />
+          <ExperienceComponent />
+          <FaqComponent />
+          <CalloutComponent />
+        </main>
+      </div>
     </div>
   </div>
 </template>
-
-<style scoped></style>
---
+++ src/components/loading/LoadingComponent.vue
@@ -0,0 +1,66 @@
+<script setup lang="ts">
+import gsap from "gsap";
+import { onMounted, useTemplateRef } from "vue";
+
+const text = useTemplateRef("text");
+const line = useTemplateRef("line");
+const upper = useTemplateRef("upper");
+const lower = useTemplateRef("lower");
+
+onMounted(() => {
+  gsap.to(text.value, {
+    duration: 1.0,
+    y: "-=40",
+    ease: "expo.out",
+  });
+  gsap.to(text.value, { opacity: 1, duration: 0.8, ease: "expo.inOut" });
+  gsap.to(line.value, {
+    duration: 1.5,
+    width: "100%",
+    ease: "expo.inOut",
+  });
+
+  gsap.to(upper.value, {
+    clipPath: "inset(0 0 100% 0)",
+    duration: 1,
+    delay: 1.5,
+    ease: "expo.inOut",
+  });
+  gsap.to(lower.value, {
+    clipPath: "inset(100% 0 0 0)",
+    duration: 1,
+    delay: 1.5,
+    ease: "expo.inOut",
+  });
+});
+</script>
+
+<template>
+  <div class="relative inset-0 z-50 h-screen w-full">
+    <div
+      ref="upper"
+      class="absolute top-0 left-0 h-[50%] w-full"
+    >
+      <div
+        class="bg-beige fixed flex h-full w-full flex-col items-center justify-center"
+      >
+        <div
+          ref="text"
+          class="absolute top-1/2 -translate-y-[calc(100%+10px)] text-xl font-bold opacity-0"
+        >
+          Welcom to NESTLY Home
+        </div>
+        <div
+          ref="line"
+          class="bg-dark-gray absolute top-1/2 left-0 h-[1px] w-0 -translate-y-[1px]"
+        ></div>
+      </div>
+    </div>
+    <div
+      ref="lower"
+      class="absolute top-1/2 left-0 h-[50%] w-full"
+    >
+      <div class="bg-beige h-full w-full"></div>
+    </div>
+  </div>
+</template>
