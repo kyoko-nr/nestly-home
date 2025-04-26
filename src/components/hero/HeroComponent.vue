95111d5776f37184291576f7dca6337d00749f1b
kyoko-nr <kyokokitagawa@me.com>
2025-04-26 18:50:42 +0900

    add scroll element

--- src/components/hero/HeroComponent.vue
+++ src/components/hero/HeroComponent.vue
@@ -5,9 +5,34 @@ import { useTemplateRef, onMounted } from "vue";
 
 const image = useTemplateRef("image");
 const title = useTemplateRef("title");
+const scroll = useTemplateRef("scroll");
+
+const setTrigger = () => {
+  gsap.fromTo(
+    scroll.value,
+    {
+      opacity: 1,
+    },
+    {
+      opacity: 0,
+      duration: 0.5,
+      ease: "expo.iuOut",
+      scrollTrigger: {
+        trigger: scroll.value,
+        start: "top 70%",
+        toggleActions: "play none none reverse",
+      },
+    },
+  );
+};
 
 onMounted(() => {
   setTimeout(() => {
+    gsap.to(image.value, {
+      scale: 1,
+      duration: 1.8,
+      ease: "expo.out",
+    });
     gsap.fromTo(
       title.value,
       {
@@ -19,14 +44,15 @@ onMounted(() => {
         y: 0,
         duration: 1.2,
         ease: "expo.out",
+        delay: 0.7,
       },
     );
-  }, 2400);
-  setTimeout(() => {
-    gsap.to(image.value, {
-      scale: 1,
-      duration: 1.8,
-      ease: "expo.out",
+    gsap.to(scroll.value, {
+      opacity: 1,
+      duration: 1,
+      ease: "expo.inOut",
+      delay: 1,
+      onComplete: setTrigger,
     });
   }, 1500);
 });
@@ -48,5 +74,51 @@ onMounted(() => {
     >
       Start living<br />smarter today
     </p>
+    <div
+      ref="scroll"
+      class="absolute bottom-0 left-1/2 -translate-y-[50px] -rotate-90 opacity-0"
+    >
+      <div class="custom-scroll relative">
+        <span>scroll</span>
+      </div>
+    </div>
   </div>
 </template>
+
+<style lang="css" scoped>
+.custom-scroll {
+  padding-left: 40px;
+  overflow: hidden;
+  &::before {
+    content: "";
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 1px;
+    background: #ffffff;
+    position: absolute;
+    opacity: 0.4;
+  }
+  &::after {
+    content: "";
+    top: 0;
+    left: 0;
+    width: 50%;
+    height: 1px;
+    background: var(--color-dark-gray);
+    position: absolute;
+    translate: -100% 0;
+    animation: scroll 1.8s var(--ease-in-out-expo) infinite;
+    opacity: 1;
+  }
+}
+
+@keyframes scroll {
+  0% {
+    translate: 200% 0;
+  }
+  100% {
+    translate: -100% 0;
+  }
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import "./assets/styles/main.css";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
 
 createApp(App).mount("#app");
